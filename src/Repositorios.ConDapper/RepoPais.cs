4852cff920ecfec0a1aa82fff5dbc207ff27a5e1
jhonaucachiet12-code <jhon.aucachi.et12@gmail.com>
2026-06-30 16:41:32 -0300

    Test pais

--- src/Repositorios.ConDapper/RepoPais.cs
+++ src/Repositorios.ConDapper/RepoPais.cs
@@ -1,10 +1,10 @@
-using Biblioteca.Repositorios;
+using Biblioteca;
 using Biblioteca.Entidades;
 using Dapper;
 using System.Data;
 
 namespace Repositorios.ConDapper;
-public class RepoPais : RepoDapper
+public class RepoPais : RepoDapper , IRepoPais
 {
     public RepoPais(IDbConnection conexion)
         : base(conexion) {}
@@ -24,5 +24,29 @@ public class RepoPais : RepoDapper
                         ORDER BY nombre ASC";
         var pais = _conexion.QueryFirstOrDefault<Pais>(consulta, new { idPais = idPais });
         return pais;
+    }
+
+    public void CrearPais(Pais pais)
+    {
+        //Creamos la lista de parametros y la asignamos
+        var parametros = new DynamicParameters();
+        parametros.Add("unIdPais", direction: ParameterDirection.Output);
+        parametros.Add("nombrePais", pais.Nombre);
+        parametros.Add("nombreDt", pais.NombreEntrenador);
+        parametros.Add("unGrupo", pais.Grupo);
+        parametros.Add("unLenguaje", pais.Lenguaje);
+        parametros.Add("unaPoblacion",((uint)pais.Poblacion));
+        parametros.Add("unaCapital ", pais.Capital);
+        parametros.Add("unHimno", pais.Himno);
+        parametros.Add("unaBandera", pais.Bandera);
+        parametros.Add("unaCamisetaOficial", pais.CamisetaOficial);
+        parametros.Add("UnDatoCurioso", pais.Datocurioso);
+        
+
+        //Ejecutamos el SP del MySQL
+        _conexion.Execute("altaPais", parametros, commandType: CommandType.StoredProcedure);
+
+        //Recuperamos el parametro marcado como OUT y lo asignamos a nuestro objeto C#
+        pais.IdPais = parametros.Get<byte>("unIdPais");
     }
 }
---
+++ src/TestRepoDappper/TestRepoPais.cs
@@ -0,0 +1,58 @@
+﻿using Biblioteca;
+using Biblioteca.Entidades;
+using MySql.Data.MySqlClient;
+using Repositorios.ConDapper;
+namespace TestRepoDappper;
+
+
+public class TestRepoPais
+{
+    private IRepoPais _repo;
+
+    public TestRepoPais()
+    {
+        var cadena = "Server=localhost;Database=bd_Mundial22;Uid=5to_agbd;Pwd=Trigg3rs!;";
+        var conexion = new MySqlConnection(cadena);
+        _repo = new RepoPais(conexion);
+    }
+    
+    [Fact]
+    public void TraerPaises()
+    {
+        var paises = _repo.ObtenerPaises();
+
+        Assert.Contains(paises, p => p.Nombre == "Argentina");
+        Assert.Contains(paises, p => p.Nombre == "Francia");
+    }
+    [Fact]
+    public void CrearBurkinafaso()
+    {
+        var burkinafaso = new Pais()
+        {
+            Grupo = 'F',
+            Nombre = "Burkinafaso",
+            NombreEntrenador = "Caruso Lombardi",
+            Lenguaje = "Frances",
+            Poblacion = 24100000,
+            Capital = "Uagadugú",
+            Himno =  null,
+            Bandera = null,
+            CamisetaOficial = null,
+            Datocurioso = "Exite"
+
+        };
+
+        var paisNoExiste = _repo.ObtenerPaises().
+                            FirstOrDefault(p => p.Nombre == "Burkinafaso");
+
+        Assert.Null(paisNoExiste);
+
+        _repo.CrearPais(burkinafaso);
+
+        var burkinaRepo = _repo.ObtenerPaises().
+                            FirstOrDefault(p => p.Nombre == "Burkinafaso");
+        Assert.NotNull(burkinaRepo);
+        Assert.Equal("Caruso Lombardi", burkinaRepo.NombreEntrenador);
+    }
+}
+
